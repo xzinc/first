f2d2fee483ab3e8a76d603d6fe9651e223ef2bf5
Learner <64130518+xzinc@users.noreply.github.com>
2022-01-29 20:52:06 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,8 +7,8 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # Basics
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
+API_ID = 7326452 #config("API_ID", default=None, cast=int)
+API_HASH = a865401e13d06664d7ffa3558f8e2940 #config("API_HASH", default=None)
+BOT_TOKEN = 2137176477:AAGg7HO6ocUL3fauzemTe1CqP0_XInP1bos#config("BOT_TOKEN", default=None)
 
 midway = TelegramClient('midwayx', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
